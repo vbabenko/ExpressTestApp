a8511f2f046f82dd99d039e97b5250e3df82d919
Vladyslav Babenko <babenko.vladyslav@gmail.com>
2015-08-20 00:01:06 +0300

    - added basic routes

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello world!!!');
+var fs = require('fs');
+var _ = require('lodash');
+var users = [];
+
+fs.readFile('users.json', function(err, data) {
+  if (err) throw err;
+
+  JSON.parse(data).forEach(function(user) {
+    user.name.full = _.startCase(user.name.first + " " + user.name.last);
+    users.push(user);
+  });
+
 });
 
-app.get('/yo', function(req, res) {
-  res.send('YO MAN!');
+app.get('/', function(req, res) {
+  var buffer = '';
+  users.forEach(function(user) {
+    buffer += '<a href="/' + user.username + '">' + user.name.full + '</a>' + '<br />'
+  });
+  res.send(buffer);
+});
+
+app.get('/:username', function(req, res) {
+  var username = req.params.username;
+  res.send('Hello ' + username);
 });
 
 var server = app.listen(3000, function() {
